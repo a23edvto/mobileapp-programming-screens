7e75c228252e45529415a21cf2114f38daae7327
Edvaure04 <edvinht@gmail.com>
2024-04-15 16:12:17 +0200

    Skapat knapp och den kan lyssna

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bytSidaKnapp = findViewById(R.id.button);
+        bytSidaKnapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("<===", "Klickat");
+            }
+        });
     }
 }
