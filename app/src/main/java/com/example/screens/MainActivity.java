f3cc808227564f5d35533095f2c68984136b0908
Edvaure04 <edvinht@gmail.com>
2024-04-19 16:18:06 +0200

    Gjorde en widget som visade intent data samt debugade då det blev fel

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.screens;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +11,18 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView myTextView = (TextView) findViewById(R.id.myTextView);
+        String name = null;
+
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String name = extras.getString("name");
+            name = extras.getString("name");
+            name = "intent".concat(name);
+            Log.d("==>", name);
+            Log.d("==>", "" + myTextView);
+            myTextView.setText(name);
         }
+
     }
 }
